02897693f3c46e8710b077f6f29717e79861dcab
devprakashduve <dev786.1@gmail.com>
2020-01-05 07:09:03 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { appRoutingModule } from './app.routing';
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 import { DayOneComponent } from './day-one';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,appRoutingModule ],
   declarations: [ AppComponent, HelloComponent, DayOneComponent ],
   bootstrap:    [ AppComponent ]
 })
